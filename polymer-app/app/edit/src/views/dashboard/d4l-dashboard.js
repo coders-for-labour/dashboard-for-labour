d61abf66f56c85157fbce646ab1ff78807d09098
Tom Cahill <tomc@wearelighten.co.uk>
2017-05-08 14:53:59 +0100

    - REFACTOR: Changed card layout on dashboard

--- polymer-app/app/edit/src/views/dashboard/d4l-dashboard.js
+++ polymer-app/app/edit/src/views/dashboard/d4l-dashboard.js
@@ -18,7 +18,18 @@ Polymer({
       value: function () { return {}; }
     },
   },
+
   attached: function() {
 
+  },
+
+  __viewTwibbyn: function () {
+    this.fire('view-entity', '/twibbyn');
+  },
+  __viewThunderclap: function () {
+    this.fire('view-entity', '/thunderclap');
+  },
+  __viewMemes: function () {
+    this.fire('view-entity', '/memes');
   }
 });
