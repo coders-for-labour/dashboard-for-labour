a57696778962f73f06486584cb1a9f180de80251
Tom Cahill <tomc@wearelighten.co.uk>
2017-05-30 16:45:50 +0100

    - INIT: Thunderclap UI (card, detail and view)

---
+++ polymer-app/app/edit/src/views/thunderclap/d4l-thunderclap-detail.js
@@ -0,0 +1,57 @@
+Polymer({
+  is: 'd4l-thunderclap-detail',
+  behaviors: [
+    Polymer.D4LLogging
+  ],
+  properties: {
+    logLevel: {
+      type: Number,
+      value: 4
+    },
+    auth: {
+      type: Object,
+      notify: true
+    },
+    db: {
+      type: Object,
+      notify: true
+    },
+    metadata: {
+      type: Object,
+      notify: true
+    },
+    campaign: {
+      type: Object,
+      observer: '__campaignChanged'
+    },
+    __selectedPlatform: {
+      type: String,
+      value: '',
+      observer: '__selectedPlatformChanged'
+    },
+  },
+
+  __campaignChanged: function(){
+    const campaignId = this.get('campaign.id');
+
+    if (!campaignId) {
+      this.__silly('__campaignChanged', 'Trying to link paths with no campaign id');
+      return;
+    }
+
+    let metaData = this.get(['db.campaign.metadata', campaignId]);
+    if (!metaData) {
+      this.__silly('__campaignChanged', 'Init default metadata for', campaignId);
+      const metaDefault = Object.assign({}, {
+        __populate__: true,
+        images: []
+      });
+      this.set(['db.campaign.metadata', campaignId], metaDefault);
+    }
+
+    this.set('metadata', this.get(['db.campaign.metadata', campaignId]));
+    this.__silly('__campaignChanged', 'metadata linking path for', campaignId);
+    this.linkPaths('metadata', `db.campaign.metadata.${campaignId}`);
+  }
+
+});
---
+++ polymer-app/app/edit/src/views/thunderclap/d4l-thunderclap.js
@@ -0,0 +1,65 @@
+Polymer({
+  is: 'd4l-thunderclap',
+  behaviors: [
+    Polymer.D4LLogging,
+    Polymer.D4LViewList
+  ],
+  properties: {
+    logLevel: {
+      type: Number,
+      value: 3
+    },
+    db: {
+      type: Object,
+      notify: true
+    },
+    auth: {
+      type: Object,
+      notify: true
+    },
+    campaign: {
+      type: Object
+    },
+    campaigns: {
+      type: Array
+    },
+
+    __openPostDialog: {
+      type: Boolean,
+      value: false
+    },
+
+    __pageTitle: {
+      type: String,
+      value: 'Images for Labour',
+      computed: '__computePageTitle(__selectedItem)'
+    },
+
+    __campaignsQuery: {
+      type: Object,
+      computed: '__computeCampaignsQuery(db.campaign.data.*)'
+    }
+  },
+
+  __computeCampaignsQuery: function () {
+    return {
+      type: {
+        $eq: 'thunderclap'
+      },
+      status: {
+        $eq: 'published'
+      }
+    }
+  },
+
+  __computePageTitle: function (campaign) {
+    let title = 'Thunderclap';
+
+    if (campaign && campaign.name) {
+      title = `${title} - ${campaign.name}`
+    }
+
+    return title;
+  }
+
+});
