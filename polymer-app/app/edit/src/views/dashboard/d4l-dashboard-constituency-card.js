4c49ecf917efda3204a2f31887e9afed004dda07
Chris Bates-Keegan <chris@wearelighten.co.uk>
2019-11-10 21:50:58 +0000

    REFACTORED: Constituencies can now be accessed by the home page tile. Searching for a post code will take the user to a Constituency topic page (if a topic doesn't exist, one is created).

--- polymer-app/app/edit/src/views/dashboard/d4l-dashboard-constituency-card.js
+++ polymer-app/app/edit/src/views/dashboard/d4l-dashboard-constituency-card.js
@@ -1,12 +1,17 @@
 Polymer({
   is: 'd4l-dashboard-constituency-card',
   behaviors: [
-    D4L.Logging
+    D4L.Logging,
+    D4L.Topic.Helpers
   ],
   properties: {
     logLevel: {
       type: Number,
       value: 4
+    },
+    db: {
+      type: Object,
+      notify: true
     },
     auth: {
       type: Object,
@@ -43,15 +48,56 @@ Polymer({
   ],
 
   __onUserMetadataChanged: function() {
-    if (this.get('constituency') || !this.get('auth.metadata.constituencyName')) {
+    // if (this.get('constituency') || !this.get('auth.metadata.constituencyName')) {
+    //   return;
+    // }
+    // this.set('__rqParams.name', this.get('auth.metadata.constituencyName'));
+    // this.$.findConstituency.generateRequest();
+  },
+
+  viewTopicConstituency: function() {
+    const topics = this.get('db.topic.data');
+    if (!topics) throw new Error('Missing TOPIC database');
+    const constituency = this.get('constituency');
+    if (!constituency) {
+      this.__warning(`Missing constituency`);
       return;
     }
-    this.set('__rqParams.name', this.get('auth.metadata.constituencyName'));
-    this.$.findConstituency.generateRequest();
+    let rootTopic = topics.find(t => !t.parentId && t.constituencyPano === constituency.pano );
+    if (!rootTopic) {
+      const dbFactory = this.get('db.Factory');
+      const topic = dbFactory.create('topic');
+      topic.name = constituency.name;
+
+      const r17 = constituency.results['2017'].results;
+      const mp = r17[0];
+      const labourIdx = r17.findIndex(mp => mp.party === 'Labour');
+      const labour = r17[labourIdx];
+      const labourBehind = r17.reduce((behind, mp, idx) => {
+        if (idx >= labourIdx) return behind;
+        behind += mp.ahead;
+        return behind;
+      }, 0)
+      if (labour !== mp) {
+        topic.description = `
+          This seat is currently held by ${mp.party} with a majority of ${mp.ahead}. The MP is ${mp.name}. The Labour MP in the 2017 election was ${labour.name}. We need ${labourBehind} votes to win this seat. 
+        `;
+      } else {
+        topic.description = `
+          This seat is currently held by Labour with a majority of ${mp.ahead}. Your MP is ${mp.name}.
+        `;
+      }
+      topic.constituencyPano = constituency.pano;
+      this.__info(topic);
+      this.push('db.topic.data', topic);
+      rootTopic = topic;
+    }
+
+    this.viewTopic(rootTopic.id);
   },
 
   __resetConstituency: function() {
-    this.set('auth.metadata.constituencyName', '');
+    // this.set('auth.metadata.constituencyName', '');
     this.set('constituency', null);
     this.set('__rqParams.name', '');
     this.set('__rqParams.postcode', '');
@@ -62,24 +108,24 @@ Polymer({
       return;
     }
 
-    this.set('auth.metadata.postCode', this.get('__rqParams.postcode'));
+    // this.set('auth.metadata.postCode', this.get('__rqParams.postcode'));
     this.$.findConstituency.generateRequest();
   },
 
   __onRqResponse: function(response) {
-    this.__debug(this.get('auth.user'));
+    this.__silly(this.get('auth.user'));
     this.__debug(response);
     if (!response) {
       return;
     }
 
     this.set('constituency', response);
-    this.linkPaths('mp', 'constituency.results.2015.results.0');
-    this.set('mp', this.get('constituency.results.2015.results.0'));
-    if (this.get('auth.metadata.constituencyName')) {
-      return;
-    }
-    this.set('auth.metadata.constituencyName', response.name);
+    // this.linkPaths('mp', 'constituency.results.2015.results.0');
+    // this.set('mp', this.get('constituency.results.2015.results.0'));
+    // if (this.get('auth.metadata.constituencyName')) {
+    //   return;
+    // }
+    // this.set('auth.metadata.constituencyName', response.name);
   },
 
   __computeHasConstituency: function() {
