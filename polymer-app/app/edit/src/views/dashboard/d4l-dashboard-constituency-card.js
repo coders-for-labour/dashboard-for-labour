dcebbf31138d4dfbe63bce7de5c777dfaa4d7c34
Tom Cahill <me@tomcahill.co.uk>
2019-11-15 09:04:12 +0000

    - FIX: Unable to view topics from constituency lookup

--- polymer-app/app/edit/src/views/dashboard/d4l-dashboard-constituency-card.js
+++ polymer-app/app/edit/src/views/dashboard/d4l-dashboard-constituency-card.js
@@ -44,45 +44,20 @@ Polymer({
   viewTopicConstituency: function() {
     const topics = this.get('db.topic.data');
     if (!topics) throw new Error('Missing TOPIC database');
+
     const constituency = this.get('constituency');
     if (!constituency) {
       this.__warning(`Missing constituency`);
       return;
     }
-    let rootTopic = topics.find(t => !t.parentId && t.constituencyPano == constituency.pano );
-    this.__silly(topics, constituency, rootTopic);
-    // if (!rootTopic) {
-    //   const dbFactory = this.get('db.Factory');
-    //   const topic = dbFactory.create('topic');
-    //   topic.name = constituency.name;
 
-    //   const r17 = constituency.results['2017'].results;
-    //   const mp = r17[0];
-    //   const labourIdx = r17.findIndex(mp => mp.party === 'Labour');
-    //   const labour = r17[labourIdx];
-    //   const labourBehind = r17.reduce((behind, mp, idx) => {
-    //     if (idx >= labourIdx) return behind;
-    //     behind += mp.ahead;
-    //     return behind;
-    //   }, 0)
-    //   if (labour !== mp) {
-    //     topic.description = `
-    //       This seat is currently held by ${mp.party} with a majority of ${mp.ahead}. The MP is ${mp.name}. The Labour MP in the 2017 election was ${labour.name}. We need ${labourBehind} votes to win this seat. 
-    //     `;
-    //   } else {
-    //     topic.description = `
-    //       This seat is currently held by Labour with a majority of ${mp.ahead}. Your MP is ${mp.name}.
-    //     `;
-    //   }
-    //   topic.constituencyPano = constituency.pano;
+    const topic = topics.find(t => t.constituencyPano == constituency.pano );
+    if (!topic) {
+      this.__warning(`Unable to find topic for constituency ${constituency.pano}`);
+      return;
+    }
 
-    //   this.__silly(topic);
-
-    //   this.push('db.topic.data', topic);
-    //   rootTopic = topic;
-    // }
-
-    this.viewTopic(rootTopic.id);
+    this.viewTopic(topic.id);
   },
 
   __resetConstituency: function() {
